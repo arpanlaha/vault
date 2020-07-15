6a763b9d666d78a906ebc127001dd5d29bc19f5f
Arpan Laha <arpanlaha99@gmail.com>
2020-07-15 18:49:59 +0000

    use actix .json method

--- api/src/server/crates.rs
+++ api/src/server/crates.rs
@@ -5,16 +5,13 @@ pub async fn get_transitive_dependencies_by_crate_id(
     req: HttpRequest,
     data: Data<AppState>,
 ) -> impl Responder {
-    HttpResponse::Ok().content_type("application/json").body(
-        serde_json::to_string(
-            &data.graph.read().await.transitive_dependencies(
-                req.match_info()
-                    .get("crate_id")
-                    .expect("crate_id not provided")
-                    .parse()
-                    .expect("Unable to parse crate_id as integer"),
-            ),
-        )
-        .expect("Unable to serialize crates"),
+    HttpResponse::Ok().json(
+        &data.graph.read().await.transitive_dependencies(
+            req.match_info()
+                .get("crate_id")
+                .expect("crate_id not provided")
+                .parse()
+                .expect("Unable to parse crate_id as integer"),
+        ),
     )
 }
