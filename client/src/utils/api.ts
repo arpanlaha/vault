98170b3390ce1c0582972f7b12691e8cc25c0700
Arpan Laha <arpanlaha99@gmail.com>
2020-07-22 18:17:23 +0000

    use env var for backend url

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { Crate, DependencyGraph } from "./types";
 
-const API_URL = "https://vaultapi.arpanlaha.com";
+const API_URL = process.env.GATSBY_VAULT_API_URL;
 
 type Response<T> =
   | {
