5b8e0ff9f27964faae11bb4dd1ac0e12955232fd
Arpan Laha <arpanlaha99@gmail.com>
2020-08-01 22:21:55 +0000

    update benchmark tests

--- graph/tests/bench_search.rs
+++ graph/tests/bench_search.rs
@@ -27,7 +27,7 @@ fn bench_search_crates_act(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_search_crates_actix_web(b: &mut Bencher) {
+fn bench_search_crates_actix(b: &mut Bencher) {
     let crate_names = GRAPH.crate_names();
     let crates = GRAPH.crates();
     b.iter(|| crate_names.search("actix", crates));
@@ -44,21 +44,21 @@ fn bench_search_crates_random(b: &mut Bencher) {
 fn bench_search_categories_a(b: &mut Bencher) {
     let category_names = GRAPH.category_names();
     let categories = GRAPH.categories();
-    b.iter(|| category_names.search("a", categories));
+    b.iter(|| category_names.search("A", categories));
 }
 
 #[bench]
 fn bench_search_categories_web(b: &mut Bencher) {
     let category_names = GRAPH.category_names();
     let categories = GRAPH.categories();
-    b.iter(|| category_names.search("web", categories));
+    b.iter(|| category_names.search("Web", categories));
 }
 
 #[bench]
 fn bench_search_categories_async(b: &mut Bencher) {
     let category_names = GRAPH.category_names();
     let categories = GRAPH.categories();
-    b.iter(|| category_names.search("async", categories));
+    b.iter(|| category_names.search("Async", categories));
 }
 
 #[bench]
