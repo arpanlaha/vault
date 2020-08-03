37225ac9425d16a2f00e832a4aa73cbef1266e50
Arpan Laha <arpanlaha99@gmail.com>
2020-08-03 21:57:03 +0000

    update install script

--- api/install.sh
+++ api/install.sh
@@ -2,9 +2,10 @@ cargo build --release
 sudo cp target/release/vault_api /usr/sbin/vault_api
 sudo cp config/systemd/vault_api.service /etc/systemd/system/vault_api.service
 sudo cp config/syslog/vault_api.conf /etc/rsyslog.d/vault_api.conf
-sudo cp config/cron/vault_api /etc/cron.daily/vault_api
 sudo mkdir /var/log/vault_api
 sudo chown syslog:adm /var/log/vault_api
+sudo cp config/cron/vault_api /etc/cron.daily/vault_api
+sudo chmod 755 /etc/cron.daily/vault_api
 sudo systemctl daemon-reload
 sudo systemctl restart rsyslog
 sudo systemctl restart cron
