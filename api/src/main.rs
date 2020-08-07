80ab018f1e078b5084ec642b37def76dac9a61a5
Arpan Laha <arpanlaha99@gmail.com>
2020-08-07 17:45:05 +0000

    add compression

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,11 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
-// use env_logger::Env;
+use env_logger::Env;
 use parking_lot::RwLock;
 use std::env;
 use std::sync::Arc;
 use vault_api::routes;
 use vault_graph::Graph;
-use warp::Filter;
 
 #[tokio::main]
 async fn main() {
@@ -31,14 +30,13 @@ async fn main() {
 
     let app_state = Arc::new(RwLock::new(Graph::new().await));
 
-    // let app_wrapper = warp::any()
-    //     .with(warp::cors().allow_any_origin())
-    //     .with(warp::body::json());
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
 
     warp::serve(
         routes::get(app_state.clone())
             .with(warp::cors().allow_any_origin())
-            .with(warp::compression::brotli()),
+            .with(warp::log("info"))
+            .with(warp::compression::gzip()),
     )
     .run((address, port))
     .await;
