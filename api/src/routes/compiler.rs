45d41cb501602f561c7e7480e7f4373cd945e827
Arpan Laha <arpanlaha99@gmail.com>
2020-08-19 06:44:04 +0000

    add compiler-related route files

---
+++ api/src/routes/compiler.rs
@@ -0,0 +1,56 @@
+use super::utils::State;
+use warp::{Filter, Rejection, Reply};
+
+/// Wraps all compiler-related routes.
+pub fn routes(state: State) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    get_targets(state.clone()).or(get_cfg_names(state.clone()))
+}
+
+/// Returns a list of targets.
+fn get_targets(state: State) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    warp::path!("compiler" / "targets")
+        .and(warp::get())
+        .and_then(move || handlers::get_targets(state.clone()))
+}
+
+/// Returns a list of cfg names.
+fn get_cfg_names(state: State) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    warp::path!("compiler" / "cfg_names")
+        .and(warp::get())
+        .and_then(move || handlers::get_cfg_names(state.clone()))
+}
+
+mod handlers {
+    use super::State;
+    use serde::Serialize;
+    use warp::{reply, Rejection, Reply};
+
+    /// Returns a list of targets.
+    pub async fn get_targets(state: State) -> Result<impl Reply, Rejection> {
+        let graph = state.read();
+
+        let mut targets: Vec<&String> = graph.targets().keys().collect();
+        targets.sort_unstable();
+
+        Ok(reply::json(&TargetList { targets }))
+    }
+
+    /// Returns a list of cfg names.
+    pub async fn get_cfg_names(state: State) -> Result<impl Reply, Rejection> {
+        Ok(reply::json(&CfgNameList {
+            cfg_names: state.read().cfg_names().iter().collect(),
+        }))
+    }
+
+    /// A struct for sending a response containing a list of targets.
+    #[derive(Serialize)]
+    pub struct TargetList<'a> {
+        pub targets: Vec<&'a String>,
+    }
+
+    /// A struct for sending a response containing a list of cfg names.
+    #[derive(Serialize)]
+    pub struct CfgNameList<'a> {
+        pub cfg_names: Vec<&'a String>,
+    }
+}
