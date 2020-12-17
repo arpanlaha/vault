054a03844bcddf8a681ca0adcc34b9b5a691dfe8
Arpan Laha <arpanlaha99@gmail.com>
2020-12-17 21:12:31 +0000

    remove client gitignore

--- cra/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import CratePanelBody from "./CratePanelBody";
-import ForceGraphWrapper from "./ForceGraphWrapper";
-import Head from "./Head";
-import Sidebar from "./Sidebar";
-
-export { CratePanelBody, ForceGraphWrapper, Head, Sidebar };
