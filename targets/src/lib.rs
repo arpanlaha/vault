9505a606953be29f76a83cc514504b48eed6d7f7
Arpan Laha <arpanlaha99@gmail.com>
2023-02-10 08:27:32 +0000

    remove comments

--- targets/src/lib.rs
+++ targets/src/lib.rs
@@ -36,38 +36,4 @@ pub fn get_targets() -> BTreeMap<String, Vec<Cfg>> {
             )
         })
         .collect()
-    // spec::get_targets()
-    //     .map(|target_triple| {
-    //         let target =
-    //             Target::search(&TargetTriple::from_triple(target_triple.as_str())).unwrap();
-    //         (
-    //             target_triple,
-    //             vec![
-    //                 Cfg::KeyPair(String::from("target_arch"), target.arch),
-    //                 Cfg::KeyPair(String::from("target_endian"), target.target_endian),
-    //                 Cfg::KeyPair(String::from("target_env"), target.target_env),
-    //                 Cfg::KeyPair(String::from("target_os"), target.target_os),
-    //                 Cfg::KeyPair(
-    //                     String::from("target_pointer_width"),
-    //                     target.target_pointer_width,
-    //                 ),
-    //                 Cfg::KeyPair(String::from("target_vendor"), target.target_vendor),
-    //             ],
-    //         )
-    //     })
-    //     .collect()
 }
-
-// pub fn get_targets() {
-//     // println!("{:?}", TARGETS);
-//     for target in TARGETS {
-//         println!(
-//             "{:?}",
-//             Target::search(
-//                 &TargetTriple::from_triple(target),
-//                 &Path::new("/").to_path_buf()
-//             )
-//             .is_err()
-//         );
-//     }
-// }
